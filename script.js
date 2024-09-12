95d56ce2403e6edd4af90a3275d9e6ae80f98d5a
JORGOX <ja549655@gmail.com>
2024-09-12 16:13:29 -0400

    Actualizacion, agregado de apartado login

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+const Inscribete_Buton =document.getElementById("Inscribete_Buton")
+const Body1 = document.getElementById("Body1")
+const Body2 = document.getElementById("Body2")
+
+Inscribete_Buton.addEventListener("click",()=>{
+    if(Body1.style.display==="none")
+    {
+        Body1.style.display ="block"
+        Body2.style.display ="none"
+    }
+    else{
+        Body1.style.display = 'none'
+         Body2.style.display ="block"
+    }
+});
